be357be056d7ab46001e09c5bcff7880ab5450e5
Jacob Santoni <jlsantoni2@gmail.com>
2020-09-18 16:52:56 -0400

    #16 Added Testing Framework and MongoDB Interface Imported these so we can eventually write a test to make sure that MongoDB (among others) is properly running. Also, added a way to interface with MongoDB from ASP.NET.

---
+++ ColossalTesting/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ColossalTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
