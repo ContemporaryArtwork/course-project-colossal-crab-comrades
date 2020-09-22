253060c8d68efa8706f155bff64186269926aa54
jmlacey <jmlacey@buffalo.edu>
2020-09-22 16:05:11 -0400

    #9 Created base UI for the start menu. Partially complete because #10 and #12 depend on #9 being started. No foreseeable side effects yet.

---
+++ ColossalGame/ColossalGame/ClientApp/src/components/MainMenu.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import "./MainMenu.css";
+
+
+import MainBackground from "../assets/mainMenu/mainBackground.jpg";
+import BGVideo from "../assets/mainMenu/BGVideo.mp4";
+
+export default class MainMenu extends React.Component {
+    state = {
+        test: 0
+    };
+
+    render() {
+        return (
+            <body>
+                <section className="showcase">
+                    <div className="video-container">                        
+                        <video src={BGVideo} loop autoPlay muted />
+                    </div>
+
+
+                    <div className="content">
+                        <h1>Pre Alpha CSE 442 Project</h1>
+                        <h3>Founders (A-Z): Eoghan Mccarroll, Jacob Santoni, Joshua Lacey, Kyle Pellechia, Zachary Wagner </h3>
+                        <a href="#about" className="btn">Log In / Create Account</a>
+                        
+                    </div>
+                </section>
+
+            
+                <section id="about">
+                    <h1>About</h1>
+                    <p>Yee yee</p>
+                    <h2> haha yes yes -will probably get rid of this section, idk it looks cool tho.</h2>
+                </section>
+               
+
+
+            </body>
+        );
+    }
+}
