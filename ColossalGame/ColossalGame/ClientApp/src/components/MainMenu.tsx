c55e6a25bdf8abed0e33fa7ab1699ef49be9f154
jmlacey <jmlacey@buffalo.edu>
2020-09-23 18:52:00 -0400

    #9 Implemented base settings button UI so that #10 can be complete. Needed for #10

--- ColossalGame/ColossalGame/ClientApp/src/components/MainMenu.tsx
+++ ColossalGame/ColossalGame/ClientApp/src/components/MainMenu.tsx
@@ -1,41 +1,45 @@
 import * as React from 'react';
 import "./MainMenu.css";
 
-
 import MainBackground from "../assets/mainMenu/mainBackground.jpg";
 import BGVideo from "../assets/mainMenu/BGVideo.mp4";
+import SettingsButton from "../assets/mainMenu/Settings Button.png";
+import SettingsPanel from "../assets/mainMenu/SettingsPanel.png";
 
 export default class MainMenu extends React.Component {
     state = {
-        test: 0
+        settingsActivated: false
     };
 
+    ToggleSettings() {                
+        var settingsPanel = document.getElementsByClassName("settings") as HTMLCollectionOf<HTMLElement>; 
+        if (settingsPanel[0].style.display === "none") {
+            settingsPanel[0].style.display = "block";
+        } else {
+            settingsPanel[0].style.display = "none";
+        }       
+    }
+        
+        
     render() {
         return (
             <body>
-                <section className="showcase">
+                <section className="enclosing">
+
+                    <button className="settingsButton" onClick={this.ToggleSettings}>
+                        <img src={SettingsButton} />
+                    </button>
+                    <div className="settings"> <img src={SettingsPanel} />  </div>
+
                     <div className="video-container">                        
                         <video src={BGVideo} loop autoPlay muted />
                     </div>
 
-
                     <div className="content">
                         <h1>Pre Alpha CSE 442 Project</h1>
-                        <h3>Founders (A-Z): Eoghan Mccarroll, Jacob Santoni, Joshua Lacey, Kyle Pellechia, Zachary Wagner </h3>
-                        <a href="#about" className="btn">Log In / Create Account</a>
-                        <a href="#about" className="btn">Settings</a>
+                        <h3>Founders (A-Z): Eoghan Mccarroll, Jacob Santoni, Joshua Lacey, Kyle Pellechia, Zachary Wagner </h3>                     
                     </div>
                 </section>
-
-            
-                <section id="about">
-                    <h1>About</h1>
-                    <p>Yee yee</p>
-                    <h2> haha yes yes -will probably get rid of this section, idk it looks cool tho.</h2>
-                </section>
-               
-
-
             </body>
         );
     }
