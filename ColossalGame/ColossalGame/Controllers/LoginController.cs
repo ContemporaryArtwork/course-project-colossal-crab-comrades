916a833185397c618729fbfecec481713a4acd9c
Jacob Santoni <jlsantoni2@gmail.com>
2020-09-28 15:58:36 -0400

    #24 Write abstract methods for service and controller for Kyle

---
+++ ColossalGame/ColossalGame/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ColossalGame.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ColossalGame.Controllers
+{
+    
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private LoginService ls;
+
+        public LoginController()
+        {
+            ls = new LoginService();
+        }
+    }
+}
