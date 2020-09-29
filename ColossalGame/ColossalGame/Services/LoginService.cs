c36cb213dd0aa90cf29fcda28b300e790434421d
Jacob Santoni <jlsantoni2@gmail.com>
2020-09-29 13:38:17 -0400

    #24 Implment interface for Login Service

--- ColossalGame/ColossalGame/Services/LoginService.cs
+++ ColossalGame/ColossalGame/Services/LoginService.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ColossalGame.Models;
 
 namespace ColossalGame.Services
 {
-    public class LoginService
+    public class LoginService : ILoginService
     {
+        private readonly UserService _us;
+        public LoginService(UserService us)
+        {
+            _us = us;
+        }
+
+        public string SignIn(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SignUp(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool VerifyToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ILoginService
+    {
+        public bool SignUp(string username, string password);
+        public string SignIn(string username, string password);
+        public bool VerifyToken(string token);
     }
 }
