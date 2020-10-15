2ef3ec729fa7ef371c1c30c8f5e834554fe5df33
Eoghan <eoghanmccarroll@gmail.com>
2020-10-14 22:01:06 -0400

    #37 Created SignalR Hub for Client Server Game data exchange
    This commit, establishes the hub and assigns it the route hub/gamedata . There is 4 potential actions the client can send through the hub, and currently the server just echo's the message to all clients. This will eventually send data to the interpolator that is currently being developed by another student.

---
+++ ColossalGame/ColossalGame/Models/Hubs/GameDataHub.cs
@@ -0,0 +1,34 @@
+﻿using ColossalGame.Helpers;
+using ColossalGame.Models.DTO;
+using ColossalGame.Models.Hubs.Clients;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColossalGame.Models.Hubs
+{
+    public class GameDataHub : Hub<IGameDataClient>, IGameDataMessageTypes
+    {
+        public async Task ChangeWeapon(string message)
+        {
+            await Clients.All.ReceiveString("This was your message: " + message);
+        }
+
+        public async Task ExitGame(string message)
+        {
+            await Clients.All.ReceiveString("This was your message: " + message);
+        }
+
+        public async Task FireWeapon(string message)
+        {
+            await Clients.All.ReceiveString("This was your message: " + message);
+        }
+
+        public async Task SendPositionUpdates(string message)
+        {
+            await Clients.All.ReceiveString("This was your message: " + message);
+        }
+    }
+}
