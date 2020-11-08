21f60ea1f6fa867bdbf4b1f8697579b117aadf02
Eoghan <eoghanmccarroll@gmail.com>
2020-11-07 21:05:14 -0500

    Separated Phaser logic from React Component Logic
    The way the game was setup, react's props were not accessible in the phaser update methods. In order to resolve this, I needed to extend the Phaser classes in order to pass down react props. In doing so, I've cleaned up the code by putting phaser logic in their own classes. I also separated the Phaser.Game from the Phaser.Scene. From what I've seen online this is best practice.
    
    **Note The GameStartRenderer.tsx file has been deleted in favor for GameStartRenderer2.tsx. The name of the new file, can be updated later, the 2 is to provide emphasis that this was a major change

---
+++ ColossalGame/ColossalGame/ClientApp/src/Phaser/Game.ts
@@ -0,0 +1,31 @@
+﻿import * as Phaser from 'phaser'
+import MainScene from './scenes/MainScene'
+
+class Game extends Phaser.Game {
+    public react: React.PureComponent;
+    constructor(react: React.PureComponent) {
+        const config = {
+            type: Phaser.AUTO,
+            width: window.innerWidth,
+            height: window.innerHeight,
+            physics: {
+                default: "arcade",
+                arcade: {
+                    gravity: { y: 0 },
+                    debug: false,
+                },
+            },
+            parent: "gameCanvas",
+            
+        }
+        super(config)
+        this.react = react;
+
+        const conf: Phaser.Types.Scenes.SettingsConfig = { key: 'MainScene' };
+        const s = new MainScene(conf, this);
+        this.scene.add(conf.key as string,s,true);
+
+    }
+}
+
+export default Game
---
+++ ColossalGame/ColossalGame/ClientApp/src/components/gameComponents/GameStartRenderer2.tsx
@@ -0,0 +1,47 @@
+﻿import * as React from 'react';
+import * as Phaser from 'phaser';
+import "./GameStartRenderer.css";
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { ApplicationState } from '../../store';
+import * as GameDataStore from "../../store/GameData";
+import Game from '../../Phaser/Game';
+
+
+
+type GameDataProps =
+    GameDataStore.GameDataState &
+    typeof GameDataStore.actionCreators &
+    RouteComponentProps<{}>;
+
+
+//Phaser Variables
+var game: Phaser.Game;
+
+class GameStartRenderer2 extends React.PureComponent<GameDataProps> {
+
+    constructor(props: any) {
+        super(props);
+    }
+    async componentDidMount() {
+
+        var x = this.props;
+        await this.props.initialize();
+        setTimeout(() => { this.props.tempLogin("admin1", "passworD1$"); }, 1000);
+
+        game = new Game(this);
+
+
+    }
+
+    public render() {
+        return (
+
+            <div id="gameCanvas" />);
+    }
+};
+
+export default connect(
+    (state: ApplicationState) => state.gameData,
+    GameDataStore.actionCreators
+)(GameStartRenderer2 as any);
