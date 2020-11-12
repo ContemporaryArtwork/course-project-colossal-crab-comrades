62e75aafde9698cc7639dfc4443375d8128f0336
Zach Wagner <32115756+wagnerz@users.noreply.github.com>
2020-11-11 22:55:27 -0500

    Main menu fixes to make it work better
    - Added exit buttons from the menus
    - Changed everything to state to make it so that buttons do not have to be double clicked
    - cleaned up the css file

--- ColossalGame/ColossalGame/ClientApp/src/components/MainMenu.tsx
+++ ColossalGame/ColossalGame/ClientApp/src/components/MainMenu.tsx
@@ -27,81 +27,60 @@ type GameMainMenuTogglerProps =
     typeof GameMainMenuTogglerStore.actionCreators &
     RouteComponentProps<{}>;
 
+interface MenuState {
+    showSettings?: boolean;
+    showLoadout?: boolean;
+}
 
 
-class MainMenu extends React.PureComponent<GameMainMenuTogglerProps> {
+class MainMenu extends React.PureComponent<GameMainMenuTogglerProps, MenuState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            showSettings: false,
+            showLoadout: false
+        }
+        this.ToggleSettings = this.ToggleSettings.bind(this);
+        this.OpenLoadout = this.OpenLoadout.bind(this);
+        this.ChooseHeavy = this.ChooseHeavy.bind(this);
+        this.ChooseBrawler = this.ChooseBrawler.bind(this);
+        this.ChooseBuilder = this.ChooseBuilder.bind(this);
+        this.closeOpenMenu = this.closeOpenMenu.bind(this);
+    }
+
 
     ToggleSettings() {
-        var settingsPanel = document.getElementsByClassName("settings") as HTMLCollectionOf<HTMLElement>;
-        if (settingsPanel[0].style.display === "none") {
-            settingsPanel[0].style.display = "block";
-        } else {
-            settingsPanel[0].style.display = "none";
-        }
-    }
-
-    SignIn() {
-        var log = document.getElementsByClassName("loginPanel") as HTMLCollectionOf<HTMLElement>;
-        if (log[0].style.display === "none") {
-            log[0].style.display = "block";
-        } else {
-            log[0].style.display = "none";
-        }
-    }
-
-    SignUp() {
-        var signUp = document.getElementsByClassName("registerPanel") as HTMLCollectionOf<HTMLElement>;
-        var log = document.getElementsByClassName("loginPanel") as HTMLCollectionOf<HTMLElement>;
-        if (signUp[0].style.display === "none") {
-            signUp[0].style.display = "block";
-            log[0].style.display = "none";
-        } else {
-            signUp[0].style.display = "none";
-        }
-    }
-
-    SendData() {
-        var signUp = document.getElementsByClassName("registerPanel") as HTMLCollectionOf<HTMLElement>;
-        var log = document.getElementsByClassName("loginPanel") as HTMLCollectionOf<HTMLElement>;
-        if (signUp[0].style.display === "none") {
-            signUp[0].style.display = "none";
-            log[0].style.display = "none";
-        } else {
-            signUp[0].style.display = "none";
-        }
+        this.setState({ showSettings: !this.state.showSettings });
     }
 
     OpenLoadout() {
-        var load = document.getElementsByClassName("loadout") as HTMLCollectionOf<HTMLElement>;
-        if (load[0].style.display === "none") {
-            load[0].style.display = "block";
-        } else {
-            load[0].style.display = "none";
-        }
+        this.setState({ showLoadout: !this.state.showLoadout });
+    }
+
+    closeOpenMenu() {
+        this.setState({ showLoadout: false });
+        this.setState({ showSettings: false });
     }
 
     ChooseHeavy() {
         var heav = document.getElementsByClassName("classColor") as HTMLCollectionOf<HTMLElement>;
-        var load = document.getElementsByClassName("loadout") as HTMLCollectionOf<HTMLElement>;
         heav[0].style.backgroundColor = "purple";
         heav[0].textContent = "HEAVY";
-        load[0].style.display = "none"
+        this.setState({ showLoadout: !this.state.showLoadout });
     }
 
     ChooseBrawler() {
         var braw = document.getElementsByClassName("classColor") as HTMLCollectionOf<HTMLElement>;
-        var load = document.getElementsByClassName("loadout") as HTMLCollectionOf<HTMLElement>;
         braw[0].style.backgroundColor = "red";
         braw[0].textContent = "BRAWLER";
-        load[0].style.display = "none"
+        this.setState({ showLoadout: !this.state.showLoadout });
     }
 
     ChooseBuilder() {
         var bui = document.getElementsByClassName("classColor") as HTMLCollectionOf<HTMLElement>;
-        var load = document.getElementsByClassName("loadout") as HTMLCollectionOf<HTMLElement>;
         bui[0].style.backgroundColor = "yellowgreen";
         bui[0].textContent = "BUILDER";
-        load[0].style.display = "none"
+        this.setState({ showLoadout: !this.state.showLoadout });
     }
 
     render() {
@@ -125,7 +104,7 @@ class MainMenu extends React.PureComponent<GameMainMenuTogglerProps> {
 
                         <div className="containMain">
                             <div className="content">
-                                <h1>Pre Alpha CSE 442 Project</h1>
+                                <h1 className="projectTitle">Pre Alpha CSE 442 Project</h1>
                                 <h3>Founders (A-Z): Eoghan Mccarroll, Jacob Santoni, Joshua Lacey, Kyle Pellechia, Zachary Wagner </h3>
                             </div>
                             <div>
@@ -139,60 +118,62 @@ class MainMenu extends React.PureComponent<GameMainMenuTogglerProps> {
                             </div>
                         </div>
 
+                        {this.state.showSettings &&
+                            <div className="settings"> <img src={SettingsPanel} />
+                                <span className="closeButton" onClick={this.closeOpenMenu}></span>
+                            </div>
+                        }
 
-
+                        {this.state.showLoadout &&
                         <div className="loadout">
+                            <span className="closeButton" onClick={this.closeOpenMenu}></span>
                             <div className="signContainer">
                                 <div className="classContainer">
                                     <div className="Heavy"></div>
                                     <p> Class Summary: <br />
-                                    Pros:<br />
-                                    - More Health <br />
-                                    Cons:<br />
-                                    - shorter firing range<br />
-                                    Abilities: <br />
-                                    - Bulk Up: gains a shield <br />for 5 seconds
-                                </p>
+                                        Pros:<br />
+                                        - More Health <br />
+                                        Cons:<br />
+                                        - shorter firing range<br />
+                                        Abilities: <br />
+                                        - Bulk Up: gains a shield <br />for 5 seconds
+                                    </p>
                                     <button className="loadButton" onClick={this.ChooseHeavy}>HEAVY</button>
                                 </div>
 
                                 <div className="classContainer">
                                     <div className="Brawler"></div>
                                     <p> Class Summary: <br />
-                                    Pros:<br />
-                                    - faster rate of fire <br />
-                                    Cons:<br />
-                                    - No special attributes<br />
-                                    Abilities: <br />
-                                    - Frenzy: gains rate of fire <br /> increase for 5 seconds
-                                </p>
+                                            Pros:<br />
+                                            - faster rate of fire <br />
+                                            Cons:<br />
+                                            - No special attributes<br />
+                                            Abilities: <br />
+                                            - Frenzy: gains rate of fire <br /> increase for 5 seconds
+                                    </p>
                                     <button className="loadButton" onClick={this.ChooseBrawler}>BRAWLER</button>
                                 </div>
 
                                 <div className="classContainer">
                                     <div className="Builder"></div>
                                     <p> Class Summary: <br />
-                                    Pros:<br />
-                                    - build structures to protect you<br />
-                                    Cons:<br />
-                                    - Not as much firepower <br />
-                                    Abilities: <br />
-                                    - Hammer time: No cooldown on <br /> building for 5 seconds
-                                </p>
+                                        Pros:<br />
+                                        - build structures to protect you<br />
+                                        Cons:<br />
+                                        - Not as much firepower <br />
+                                        Abilities: <br />
+                                        - Hammer time: No cooldown on <br /> building for 5 seconds
+                                    </p>
                                     <button className="loadButton" onClick={this.ChooseBuilder}>BUILDER</button>
                                 </div>
                             </div>
                         </div>
+                        }
 
-                        <div className="settings"> <img src={SettingsPanel} />  </div>
                     </div>
                 </body>
             );
         }
-        
-
-
-
     }
 }
 
